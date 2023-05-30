ea82c576bdbc3a474d73c89e67a2e1e2f696c666
Artem <cvfhn81@gmail.com>
2023-05-30 08:30:59 +0300

    don't stop button in container

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -5,6 +5,15 @@ $('.first-screen-slider').slick({
     dots: true,
     speed: 1000,
     appendArrows: ".container-arrows",
+    responsive: [
+        {
+          breakpoint: 991,
+          settings: {
+            arrows: false,
+            
+          }
+        },
+    ]
 });
 
 });
