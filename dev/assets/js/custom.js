97fb73d84932f80a2b4ca4210fefa9d8525e647b
Artem <cvfhn81@gmail.com>
2023-06-09 08:50:00 +0300

    advantages desktop done

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -42,3 +42,29 @@ document.getElementById('fileInput').onchange = function() {
   //long name
   // document.getElementById('file-name').innerHTML = this.value;
 };
+
+mobileOnlySlider(".advantages-slider", true, false, 991);
+
+function mobileOnlySlider($slidername, $dots, $arrows, $breakpoint) {
+    var slider = $($slidername);
+    var settings = {
+        mobileFirst: true,
+        dots: $dots,
+        arrows: $arrows,
+        responsive: [{
+            breakpoint: $breakpoint,
+            settings: "unslick"
+        }]
+    };
+
+    slider.slick(settings);
+
+    $(window).on("resize", function() {
+        if ($(window).width() > $breakpoint) {
+            return;
+        }
+        if (!slider.hasClass("slick-initialized")) {
+            return slider.slick(settings);
+        }
+    });
+} // Mobile Only Slider
