3f0557aa8eae5f9580efeb584aacb9133b9c5294
Artem <cvfhn81@gmail.com>
2023-07-15 00:31:05 +0300

    dilivery_services done

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,20 +1,82 @@
-$( document ).ready(function() {
+$(document).ready(function() {
 
 
-$('.first-screen-slider').slick({
-    dots: true,
-    speed: 1000,
-    appendArrows: ".container-arrows",
-    responsive: [
-        {
-          breakpoint: 991,
-          settings: {
-            arrows: false,
-            
+  $('.first-screen-slider').slick({
+      dots: true,
+      speed: 1000,
+      appendArrows: ".container-arrows",
+      responsive: [
+          {
+            breakpoint: 991,
+            settings: {
+              arrows: false,
+              
+            }
+          },
+        ]
+    });
+
+    mobileOnlySlider(".advantages-slider", true, false, 991);
+
+    function mobileOnlySlider($slidername, $dots, $arrows, $breakpoint) {
+        var slider = $($slidername);
+        var settings = {
+            mobileFirst: true,
+            dots: $dots,
+            arrows: $arrows,
+            responsive: [{
+                breakpoint: $breakpoint,
+                settings: "unslick"
+            }]
+        };
+
+        slider.slick(settings);
+
+        $(window).on("resize", function() {
+            if ($(window).width() > $breakpoint) {
+                return;
+            }
+            if (!slider.hasClass("slick-initialized")) {
+                return slider.slick(settings);
+            }
+        });
+    } // Mobile Only Slider
+
+    $('.slider-template').slick({
+      dots: false,
+      speed: 300,
+      arrows: true,
+      
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      responsive: [{
+              breakpoint: 1400,
+              settings: {
+                  slidesToShow: 2,
+                
+                  arrows: true,
+              }
+          },
+          {
+              breakpoint: 991,
+              settings: {
+                  slidesToShow: 1,
+                
+              }
+          },
+          {
+              breakpoint: 575,
+              settings: {
+                  slidesToShow: 1,
+                  arrows: false,
+                  dots: true,
+              }
           }
-        },
-    ]
-});
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+      ]
+    });
 
 });
 
@@ -37,34 +99,12 @@ if (document.querySelector('.calculation-form')) {
 
 document.getElementById('fileInput').onchange = function() {
   //short name
-  document.getElementById('file-name').innerHTML = this.files[0].name;
+document.getElementById('file-name').innerHTML = this.files[0].name;
 
   //long name
   // document.getElementById('file-name').innerHTML = this.value;
 };
 
-mobileOnlySlider(".advantages-slider", true, false, 991);
 
-function mobileOnlySlider($slidername, $dots, $arrows, $breakpoint) {
-    var slider = $($slidername);
-    var settings = {
-        mobileFirst: true,
-        dots: $dots,
-        arrows: $arrows,
-        responsive: [{
-            breakpoint: $breakpoint,
-            settings: "unslick"
-        }]
-    };
 
-    slider.slick(settings);
 
-    $(window).on("resize", function() {
-        if ($(window).width() > $breakpoint) {
-            return;
-        }
-        if (!slider.hasClass("slick-initialized")) {
-            return slider.slick(settings);
-        }
-    });
-} // Mobile Only Slider
